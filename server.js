17efefa219e0daf1018dd1ac6f709a1a9608fa4a
hadarasulin5 <hadarasulin5@gmail.com>
2026-02-22 15:04:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const SHOPIFY_API_SECRET = process.env.SHOPIFY_API_SECRET || '';
 const APP_URL = process.env.APP_URL || 'https://hfd-shipping.onrender.com';
 
 const tokenStore = {};
+if (process.env.SHOPIFY_TOKEN) {
+  tokenStore['xn-vldscdfsea5ah3ae6j.myshopify.com'] = process.env.SHOPIFY_TOKEN;
+}
 const settingsStore = {};
 
 // ===== CSP Headers =====
